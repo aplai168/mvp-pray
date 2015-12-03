a5e66ec7bebbd7dfddccb99206743ffa4c65ee55
Dan Thareja <danthareja@gmail.com>
2015-12-02 17:25:21 -0800

    Remove old comment. Code speaks for itself now

--- server/server.js
+++ server/server.js
@@ -15,20 +15,3 @@ app.listen(8000);
 
 // export our app for testing and flexibility, required by index.js
 module.exports = app;
-
-
-/* Walkthrough of the server
-
-  Express, mongoose, and our server are initialzed here
-  Next, we then inject our server and express into our config/middleware.js file for setup.
-    We also exported our server for easy testing
-
-  middleware.js requires all express middleware and sets it up
-  our authentication is set up there as well
-  we also create individual routers for are two main features, links and users
-  each feature has its own folder with a model, controller, and route file
-    the respective file is required in middleware.js and injected with its mini router
-    that route file then requires the respective controller and sets up all the routes
-    that controller then requires the respective model and sets up all our endpoints which respond to requests
-
-*/
