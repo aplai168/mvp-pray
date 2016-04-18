065c71e75bd6bbd85db724f0a6d9f023778a3a69
Dan Thareja <danthareja@gmail.com>
2016-04-18 11:52:00 -0700

    Add babel support

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,28 +1,46 @@
 'use strict';
-
 var gulp = require('gulp');
+var clean = require('gulp-clean');
+var babel = require('gulp-babel');
 var nodemon = require('gulp-nodemon');
 var KarmaServer = require('karma').Server;
 var browserSync = require('browser-sync').create();
 
-// the paths to our app files
+// The paths to our app files
 var paths = {
-  // all our client app js files, not including 3rd party js files
-  scripts: ['client/app/**/*.js'],
-  html: ['client/app/**/*.html', 'client/index.html'],
-  styles: ['client/styles/style.css'],
-  test: ['specs/**/*.js']
+  src: {
+    scripts: ['client/app/**/*.js'],
+    html: ['client/app/**/*.html', 'client/index.html'],
+    styles: ['client/styles/style.css']
+  },
+  compiled: {
+    scripts: ['compiled/app/**/*.js'],
+    html: ['compiled/app/**/*.html', 'compiled/index.html'],
+    styles: ['compiled/styles/style.css']
+  },
+  test: ['specs/**/*.js'],
+  server: 'server/server.js'
 };
 
-// any changes made to your
-// client side code will automagically refresh your page
-// with the new changes
+/***************************************
+ *                ES5
+ ***************************************/
+
+// Start our node server using nodemon
+gulp.task('serve', function () {
+  nodemon({
+    script: paths.server,
+    ignore: 'node_modules/**/*.js'
+  });
+});
+
+// Any changes made to your client side code will
+// automagically refresh your page with the new changes
 gulp.task('start', ['serve'], function () {
   browserSync.init({
     notify: true,
     injectChanges: true,
-    files: paths.scripts.concat(paths.html, paths.styles),
-    // address for server
+    files: paths.src.scripts.concat(paths.src.html, paths.src.style),
     proxy: 'localhost:8000'
   });
 });
@@ -34,13 +52,71 @@ gulp.task('karma', function (done) {
   }, done).start();
 });
 
-// start our node server using nodemon
-gulp.task('serve', function () {
+// Use ES5 by default
+gulp.task('default', ['start']);
+
+/***************************************
+ *                ES6
+ ***************************************/
+
+// Remove previously transpiled code
+gulp.task('clean', function() {
+  return gulp.src(paths.compiled.scripts)
+  .pipe(clean({read: false}));
+});
+
+// Transpile our client scripts 
+gulp.task('babel', ['clean'], function() {
+  return gulp.src('client/**/*')
+    .pipe(babel({
+      presets: ['es2015'],
+      only: paths.src.scripts
+    }))
+    .pipe(gulp.dest('compiled'));
+});
+
+// Start a babel-node server using nodemon
+gulp.task('serve:es6', ['babel'], function () {
   nodemon({
-    script: './server/server.js',
+    exec: 'babel-node',
+    script: paths.server,
     ignore: 'node_modules/**/*.js'
   });
 });
 
-gulp.task('default', ['start']);
+// Any changes made to your client side code will still
+// automagically refresh your page with the new changes,
+// after transpiling again with babel
+gulp.task('start:es6', ['serve:es6'], function () {
+  browserSync.init({
+    notify: true,
+    injectChanges: true,
+    files: paths.compiled.scripts.concat(paths.compiled.html, paths.compiled.style),
+    proxy: 'localhost:8000'
+  });
 
+  gulp.watch(paths.scripts, ['babel']);
+});
+
+// Tests can be written in ES6 too!
+gulp.task('karma:es6', function(done) {
+  new KarmaServer({
+    configFile: __dirname + '/karma.conf.js',
+    preprocessors: {
+      [paths.src.scripts]: ['babel'],
+      [paths.test]: ['babel']
+    },
+    babelPreprocessor: {
+      options: {
+        presets: ['es2015'],
+        sourceMap: 'inline'
+      },
+      filename: function (file) {
+        return file.originalPath;
+      },
+      sourceFileName: function (file) {
+        return file.originalPath;
+      }
+    }
+  }, done).start();
+});
